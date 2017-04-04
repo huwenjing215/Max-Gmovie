b1b2716698fd60e18138338f1f1bc4ac5d805a9f
huwenjing <1035344285@qq.com>
2017-04-04 08:29:39 +0800

    when created a group, join it automatically

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -21,6 +21,7 @@ before_action :authenticate_user! , only: [:new, :create, :edit, :update, :desto
     @group.user = current_user
 
     if @group.save
+      current_user.join!(@group)
       redirect_to groups_path
     else
       render :new
